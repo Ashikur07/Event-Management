40fae2de383c241c78a11e732b1773b90693900b
Ashikur07 <ashik.ict.iu@gmail.com>
2026-01-03 15:57:17 +0600

    fixed issures

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,12 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: process.env.NODE_ENV === "development", // ডেভেলপমেন্ট মোডে বন্ধ থাকবে
-  workboxOptions: {
-    disableDevLogs: true,
-  },
+  disable: process.env.NODE_ENV === "development", // ডেভেলপমেন্ট মোডে PWA বন্ধ থাকবে
+  // register: true,
+  // skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // তোমার যদি অন্য কনফিগ থাকে এখানে যোগ করতে পারো
-};
+const nextConfig = {};
 
 export default withPWA(nextConfig);
